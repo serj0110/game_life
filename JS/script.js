e4c1323c47f3dd22adf958ca44c9555439ef5cb9
Sergey <druzhinin12321@gmail.com>
2016-10-01 03:31:10 +0700

    Update
    Update app

--- JS/script.js
+++ JS/script.js
@@ -1,171 +1,137 @@
-var xLenght = 50, yLength = 50; //Количество ячеек
-var delay = 1; //Задержка
-var cell = 10; //Размерность ячейки
-var cellColor = "#000000"; //Цвет линий
-var src_0 = "#ffffff", src_1 = "#01A9DB"; //Мертвая и живая клетка
-var cellLife = [];
-
-//Функция настройки поля
-function lifeOption() {
-    var w, h, d;
-    w = document.getElementById("lifeWidth").value;
-    h = document.getElementById("lifeHeight").value;
-    d = document.getElementById("lifeDelay").value;
-    if ( w !== xLenght || h !== yLength || d !== delay) {
-        xLenght = w;
-        yLength = h;
-        delay = d;
-        for ( x = 1; x < xLenght + 1; x++) {
-        cellLife[x] = [];
-        for ( y = 1; y < yLength + 1; y++) {
-            cellLife[x][y] = 0;
-        }
-    }
-    }
-
-    var field = {
-        canvasWidth: cell * xLenght, //ширина холста
-        canvasHeight: cell * yLength, //высота холста
-        cellsNumberX: xLenght, //количество ячеек по горизонтали
-        cellsNumberY: yLength, //количество ячеек по вертикали
-        color : cellColor, //цвет линий
-        //Метод setSettings устанавливает все настройки
-        setSettings : function() {
-            canvas = document.getElementById("example");
-            // устанавливаем ширину холста
-            canvas.width = this.canvasWidth;
-            // устанавливаем высоту холста
-            canvas.height = this.canvasHeight;
-            ctx = canvas.getContext("2d");
-            ctx.strokeStyle = this.color;
-            // вычисляем ширину ячейки по горизонтали
-            lineX = cell;
-            lineY = cell;
-        },
-        drawGrid : function() {
-            // в переменной buf будет храниться начальная координата, откуда нужно рисовать линию
-            // с каждой итерацией она должна увеличиваться либо на ширину ячейки, либо на высоту
-            var buf = 0;
-            // Рисуем вертикальные линии
-            for (var i = 0; i <= this.cellsNumberX; i++) {
-                ctx.beginPath();
-                ctx.moveTo(buf, 0);
-                ctx.lineTo(buf, canvas.height);
-                ctx.stroke();
-                buf +=lineX;
-            }
-            buf = 0;
-            // Рисуем горизонтальные линии
-            for (var j = 0; j <= this.cellsNumberY; j++) {
-                ctx.beginPath();
-                ctx.moveTo(0, buf);
-                ctx.lineTo(canvas.width, buf);
-                ctx.stroke();
-                buf +=lineY;
-            }
-        }
-    }
-    field.setSettings();
-    field.drawGrid();
-}
-lifeWidth.onblur = function () {
-    var w, h, d;
-    w = document.getElementById("lifeWidth").value;
-    h = document.getElementById("lifeHeight").value;
-    d = document.getElementById("lifeDelay").value;
-    if ( w !== xLenght || h !== yLength || d !== delay) {
-        xLenght = w;
-        yLength = h;
-        delay = d;
-        for ( x = 1; x < xLenght + 1; x++) {
-        cellLife[x] = [];
-        for ( y = 1; y < yLength + 1; y++) {
-            cellLife[x][y] = 0;
-        }
-    }
-    }
-
-    var field = {
-        canvasWidth: cell * xLenght, //ширина холста
-        canvasHeight: cell * yLength, //высота холста
-        cellsNumberX: xLenght, //количество ячеек по горизонтали
-        cellsNumberY: yLength, //количество ячеек по вертикали
-        color : cellColor, //цвет линий
-        //Метод setSettings устанавливает все настройки
-        setSettings : function() {
-            canvas = document.getElementById("example");
-            // устанавливаем ширину холста
-            canvas.width = this.canvasWidth;
-            // устанавливаем высоту холста
-            canvas.height = this.canvasHeight;
-            ctx = canvas.getContext("2d");
-            ctx.strokeStyle = this.color;
-            // вычисляем ширину ячейки по горизонтали
-            lineX = cell;
-            lineY = cell;
-        },
-        drawGrid : function() {
-            // в переменной buf будет храниться начальная координата, откуда нужно рисовать линию
-            // с каждой итерацией она должна увеличиваться либо на ширину ячейки, либо на высоту
-            var buf = 0;
-            // Рисуем вертикальные линии
-            for (var i = 0; i <= this.cellsNumberX; i++) {
-                ctx.beginPath();
-                ctx.moveTo(buf, 0);
-                ctx.lineTo(buf, canvas.height);
-                ctx.stroke();
-                buf +=lineX;
-            }
-            buf = 0;
-            // Рисуем горизонтальные линии
-            for (var j = 0; j <= this.cellsNumberY; j++) {
-                ctx.beginPath();
-                ctx.moveTo(0, buf);
-                ctx.lineTo(canvas.width, buf);
-                ctx.stroke();
-                buf +=lineY;
-            }
-        }
-    }
-    field.setSettings();
-    field.drawGrid();
-}
-lifeHeight.onblur = lifeWidth.onblur;
-//-----------------------------------------------------------
-//Функция клика
-function mouseClickDown(e) {
-    if ( cellLife[Math.ceil( ( e.pageX - canvas.offsetLeft ) / cell )][Math.ceil( ( e.pageY - canvas.offsetTop ) / cell )] === 0 ) {
-        var xCellClick = Math.ceil( ( e.pageX - canvas.offsetLeft ) / cell ) * cell - 9;
-        var yCellClick = Math.ceil( ( e.pageY - canvas.offsetTop ) / cell ) * cell - 9;
-        ctx.fillStyle = src_1;
-        ctx.fillRect( xCellClick, yCellClick, cell - 2, cell - 2);
-        cellLife[Math.ceil( ( e.pageX - canvas.offsetLeft ) / cell )][Math.ceil( ( e.pageY - canvas.offsetTop ) / cell )] = 1;
-    } else {
-        var xCellClick = Math.ceil( ( e.pageX - canvas.offsetLeft ) / cell ) * cell - 9;
-        var yCellClick = Math.ceil( ( e.pageY - canvas.offsetTop ) / cell ) * cell - 9;
-        ctx.fillStyle = src_0;
-        ctx.fillRect( xCellClick, yCellClick, cell - 2, cell - 2);
-        cellLife[Math.ceil( ( e.pageX - canvas.offsetLeft ) / cell )][Math.ceil( ( e.pageY - canvas.offsetTop ) / cell )] = 0;
-    }
-}
-//-----------------------------------------------------------
-//Функция старт
-//-----------------------------------------------------------
-//Функция пауза
-//-----------------------------------------------------------
+//Глобальные переменные
+var xWidth = 20, yHeight = 20;
+var delay = 1;
+var C_layer, N_layer;
+var src_0 = "#ffffff";
+var src_1 = "#01A9DB";
+var active = 1;
+var cash;
+//-----------------------------------------------------
 //Главная функция
-function life() {
-    var x, y;
-    document.getElementById("lifeWidth").value = xLenght;
-    document.getElementById("lifeHeight").value = yLength;
-    document.getElementById("lifeDelay").value = delay;
-    for ( x = 1; x < xLenght + 1; x++) {
-        cellLife[x] = [];
-        for ( y = 1; y < yLength + 1; y++) {
-            cellLife[x][y] = 0;
+function life( first ) {
+    initSpace( xWidth, yHeight);
+    if ( first ) appSetting();
+    createField();
+    initCash();
+    unfocus();
+    setStart();
+    setPause();
+}
+//-----------------------------------------------------
+//Рисование поля
+function createField(){
+	var table = "<table id='field'>";
+	for(var y = 1;y <= yHeight;y++){
+		table += "<tr>";
+		for ( var x = 1; x <= xWidth; x++ ) {
+			var i = ( y - 1 ) * xWidth + x - 1;
+			table += "<td id="+i+" onclick='clickColor("+x+","+y+");'></td>";
+		}
+		table += "</tr>";
+	}
+	table += "</table>";
+	document.getElementById('app').innerHTML = table;
+}
+//-----------------------------------------------------
+//Изменение настроек
+function appSetting() {
+    document.getElementById("setWidth").value = xWidth;
+    document.getElementById("setHeight").value = yHeight;
+    document.getElementById("setDelay").value = delay;
+}
+//-----------------------------------------------------
+//Изменение переменной по unfocus
+function unfocus() {
+    setWidth.onblur = function () {
+        var w = document.getElementById("setWidth").value;
+        xWidth = w;
+        life(0);
+    }
+    setHeight.onblur = function () {
+        var h = document.getElementById("setHeight").value;
+        yHeight = h;
+        life(0);
+    }
+    setDelay.onblur = function () {
+        var d = document.getElementById("setDelay").value;
+        delay = d;
+    }
+}
+//-----------------------------------------------------
+//Инициализирует кэш ячеек
+function initCash() {
+    cash = new Array ( xWidth * yHeight );
+    for ( var y = 1; y <= yHeight; y++ ) {
+        for ( var x = 1; x <= xWidth; x++ ) {
+            var i = ( y - 1 ) * xWidth + x - 1;
+            cash[i] = document.getElementById(i);
+            cash[i].unselectable = true;
         }
     }
-    lifeOption();
-    canvas.onmousedown = mouseClickDown;
 }
-//-----------------------------------------------------------
+//-----------------------------------------------------
+//Инвертация состояния ячейки
+function clickColor(x, y) {
+    var i = ( y - 1 ) * xWidth + x - 1;
+    cash[i].style.backgroundColor = C_layer[ y ][ x ] ? src_0 : src_1;
+    C_layer[y][x] = C_layer[y][x] ? 0 : 1;
+}
+//-----------------------------------------------------
+//
+function initSpace(x, y) {
+    C_layer = new Array( y + 2 );
+    N_layer = new Array( y + 2 );
+    for ( var i = 0; i < y + 2; i++ ) {
+        C_layer[i] = new Array( x + 2 );
+        N_layer[i] = new Array( x + 2 );
+    }
+    for ( var i = 0; i < y + 2; i++) {
+        for ( var j = 0; j < x + 2; j++) {
+            C_layer[i][j] = N_layer[i][j] = 0;
+        }
+    }
+}
+//-----------------------------------------------------
+//
+function swapLayers(){
+	var tmp = C_layer;
+	C_layer = N_layer;
+	N_layer = tmp;
+}
+//-----------------------------------------------------
+//
+function CalcNextStep(){
+var i, N;
+for ( var y = yHeight; y--;) {
+	for ( var x = xWidth; x--;) {
+        N = C_layer[y]  [x] + C_layer[y]  [x+1] + C_layer[y]  [x+2] +
+            C_layer[y+1][x] +                     C_layer[y+1][x+2] +
+            C_layer[y+2][x] + C_layer[y+2][x+1] + C_layer[y+2][x+2];
+        N_layer[y+1][x+1] =
+            (((C_layer[y+1][x+1])&&(N == 2 || N == 3))||((!C_layer[y+1][x+1])&&(N == 3))) ? 1 : 0;
+        if( N_layer[ y + 1 ][ x + 1 ] != C_layer[ y + 1 ][ x + 1 ]) {
+            i = y * xWidth + x;
+            cash[i].style.backgroundColor = N_layer[y+1][x+1] ? src_1 : src_0;
+        }
+    }
+}
+swapLayers();
+}
+//-----------------------------------------------------
+//Начать
+function setStart( check ){
+	CalcNextStep();
+	if( check ) active = 1;
+	if( active ) window.setTimeout("setStart(0)", delay);
+}
+//-----------------------------------------------------
+//Пауза
+function setPause() {
+    active = 0;
+}
+//-----------------------------------------------------
+//
+function setClear() {
+    life(0);
+}
+//-----------------------------------------------------
